717b24ae2dbf171f347d1abc82f24e3a760c17fd
faizan1250 <mdf998865@gmail.com>
2026-03-10 16:49:35 +0000

    configured backend

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import prisma from "./config/db.ts";
+
+const app = express();
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
